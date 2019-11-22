0fa3d9ac44d597041b23b1dd83dddd8372db5b86
Ockiya Daniel <beinmonyudaniel@gmail.com>
2019-11-22 23:44:48 +0100

    Updates

---
+++ precache-manifest.86351a0c3e0f087dbb933305ff828bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1bd5e7763c92f5351fa7137b9d4adf",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "5ef6cc08d1ca2f25c777",
+    "url": "/monster-app/static/css/main.b01d40e4.chunk.css"
+  },
+  {
+    "revision": "db24dc879035f8631897",
+    "url": "/monster-app/static/js/2.a47b4d75.chunk.js"
+  },
+  {
+    "revision": "5ef6cc08d1ca2f25c777",
+    "url": "/monster-app/static/js/main.f91555b2.chunk.js"
+  },
+  {
+    "revision": "096f0bce6209bffc1ef6",
+    "url": "/monster-app/static/js/runtime-main.11bde6f8.js"
+  }
+]);
